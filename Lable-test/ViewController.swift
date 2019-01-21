9f24cb1d9cba00bf5ab283fa9f6699ffeb4b5a66
Andrey <marvel.dnepr@gmail.com>
2019-01-22 00:17:30 +0200

    Ура! Добавляется номер экрана

--- Lable-test/ViewController.swift
+++ Lable-test/ViewController.swift
@@ -8,45 +8,48 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-    var n=0
-    var someArray:[Int]=[0]
-    @IBOutlet weak var countWindow: UILabel!
-    @IBOutlet weak var clickCheck: UILabel!
-    @IBOutlet weak var viewSum: UILabel!
+class ViewController : UIViewController {
+    var n = 0
+    static var count = 0
+    var someArray : [Int] = [0]
+    @IBOutlet weak var countWindow : UILabel!
+    @IBOutlet weak var clickCheck : UILabel!
+    @IBOutlet weak var viewSum : UILabel!
     @IBAction func click() {
-        n+=1
+        n += 1
         updateTextField(n)
     }
     @IBAction func backClick() {
-        n-=1
+        n -= 1
         updateTextField(n)
     }
-    @IBAction func toZero () {
-        n=0
+    @IBAction func toZero() {
+        n = 0
         updateTextField(n)
     }
     func updateTextField(_:Int) {
-        if n==0 {clickCheck.textColor=UIColor.green} else if n%2==0 {clickCheck.textColor=UIColor.blue} else {clickCheck.textColor=UIColor.red}
-        clickCheck.text="\(n)"
+        if n == 0 {
+            clickCheck.textColor = UIColor.green
+            } else if n%2 == 0 {
+            clickCheck.textColor = UIColor.blue
+            } else {
+            clickCheck.textColor = UIColor.red
+            }
+        clickCheck.text = "\(n)"
         someArray.append(n)
     }
     @IBAction func sum() {
-        var totalSum=0
-        for a in someArray[0...someArray.count-1] { totalSum+=a }
-        viewSum.text="\(totalSum)"
+        var totalSum = 0
+        for a in someArray[0...someArray.count-1] { totalSum += a }
+        viewSum.text = "\(totalSum)"
     }
     @IBAction func newScreen(_ sender: UIButton) {
-        let storyboard=UIStoryboard(name: "Main", bundle: nil)
-        let controller=storyboard.instantiateViewController(withIdentifier: "someViewController")
-        Values.m+=1
-        self.present(controller , animated: true, completion: nil)
-        countWindow.text=Values.textScreen
+        ViewController.count += 1
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let controller : ViewController = storyboard.instantiateViewController(withIdentifier: "someViewController") as! ViewController
+        self.present(controller, animated: true, completion: nil)
+        controller.countWindow.text = "Screen № \(ViewController.count)"
     }
-  
 }
 
-class Values {
-    static var m:Int=1
-    static var textScreen:String="Screen № \(m)"
-}
+
